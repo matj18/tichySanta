fa88bc7a4df296a1596a84453a5a7fcac78f0379
Júlia Matulová <matj18@vse.cz>
2020-05-31 19:29:33 +0200

    Registrace na fb podle cviční + zapomenuté heslo

---
+++ inc/facebook.php
@@ -0,0 +1,7 @@
+<?php
+
+  $fb = new Facebook\Facebook([
+    'app_id' => '263295168214868',
+    'app_secret' => '2a02972ae5de8dac9cab78e487b160ec',
+    'default_graph_version' => 'v4.0',
+  ]);
--- inc/user.php
+++ inc/user.php
@@ -1,12 +1,14 @@
-<?php //kod z cviceni
+<?php
 
   session_start(); //spustíme session
 
   require_once 'db.php'; //načteme připojení k databázi
 
+  require_once __DIR__.'/../vendor/autoload.php';//načtení class loaderu vytvořeného composerem
+
   #region kontrola, jestli je přihlášený uživatel platný
   if (!empty($_SESSION['user_id'])){
-    $userQuery=$db->prepare('SELECT user_id FROM users WHERE user_id=:id AND active=1 LIMIT 1;');
+    $userQuery=$db->prepare('SELECT user_id FROM users WHERE user_id=:id LIMIT 1;');
     $userQuery->execute([
       ':id'=>$_SESSION['user_id']
     ]);
