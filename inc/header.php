9e034e1204d2dcbf1aa09ba5d0ea96856cdb06ad
Júlia Matulová <matj18@vse.cz>
2020-05-31 22:39:56 +0200

    uprava header.php, pridani stylu css

--- inc/header.php
+++ inc/header.php
@@ -5,10 +5,11 @@
     <meta charset="utf-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="inc/style.css">
   </head>
   <body>
     <header class="container bg-dark">
-      <h1 class="text-white py-4 px-2">Tichý Santa</h1>
+        <h1 class="text-white py-4 px-2"><a href="index.php" class="nodeco">Tichý Santa</a></h1>
 
       <div class="text-right text-white">
       <?php
