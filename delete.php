25145c75551002f541c8553b0ab4e6f48e2ed2bb
Júlia Matulová <matj18@vse.cz>
2020-06-02 03:59:55 +0200

    uprava mazani kategorii - vymena za jinou

--- delete.php
+++ delete.php
@@ -14,7 +14,6 @@ $type = $_REQUEST['type'];
 $id = $_REQUEST['id'];
 
 if ($type=='gifts') {
-    echo "<script type='text/javascript'>alert('zde1');</script>";
     $query = $db->prepare('DELETE FROM gifts WHERE gift_id = :id');
     $query->execute([
         ':id'=>$id
@@ -26,6 +25,22 @@ if ($type=='gifts') {
     $query->execute([
         ':id'=>$id
     ]);
+
+    #region obnova kategorie u prispevku
+    //najdeme nejakou kategorii
+    $catQuery=$db->prepare('SELECT * FROM categories LIMIT 1;');
+    $catQuery->execute();
+    if ($cat=$catQuery->fetch(PDO::FETCH_ASSOC)) {
+        //kdyz ji mame, dame ji prispevkum bez kategorie
+        $saveQuery=$db->prepare('UPDATE gifts SET category_id=:category WHERE category_id is NULL ;');
+        $saveQuery->execute([
+            ':category'=>$cat['category_id']
+        ]);
+    }
+
+    #endregion obnova kategorie u prispevku
+
+
     header('Location: editcategories.php');
     exit();
 }
